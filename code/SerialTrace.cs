76f13165a85d14d024f8d4bc08cd89edaf6a0e1b
Manny Haller <git@mannyhaller.com>
2015-02-15 17:52:23 -0800

    Copy from subversion

---
+++ code/SerialTrace.cs
@@ -0,0 +1,18 @@
+﻿// $URL$
+// $Id$
+
+// Copyright © Jason Curl 2012-2014
+// See http://serialportstream.codeplex.com for license details (MS-PL License)
+
+namespace RJCP.IO.Ports
+{
+    using System;
+    using System.Diagnostics;
+    using System.Text;
+
+    public partial class SerialPortStream
+    {
+        private static TraceSource m_Trace = new TraceSource("IO.Ports.SerialPortStream");
+        private static TraceSource m_TraceRT = new TraceSource("IO.Ports.SerialPortStream_ReadTo");
+    }
+}
